dd05ef61146640e29f70df89e637fb1782e77760
Sultan Saadat <sultansaadat@gmail.com>
2014-06-11 16:27:25 +0500

    changes and fixes

--- app.js
+++ app.js
@@ -4,36 +4,19 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var MongoClient = require('mongodb').MongoClient;
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var employee_details = require('./routes/employee_details');
 var employee_list = require('./routes/employee_list');
 var employee_salary_details = require('./routes/employee_salary_details');
 var employee_attendence = require('./routes/employee_attendence');
+var cons = require('consolidate');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-// html engine
-
-cons = require('consolidate'),
-
-//routes of modules
-app.get('/view/attendence', employee_attendence.viewEmployee_attendence);
-app.post('/attendence/add', employee_attendence.addAttendence);
-
-
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-app.get('/', function(req, res) {
-    res.render("index");
-});
-
-
+app.set('views', path.join(__dirname, 'views'));
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -41,46 +24,9 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-app.get('/redirect', function(req, res) {
-    res.redirect('/');
-});
-//routes here
-
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+/*routes*/
+app.get('/view/attendence', employee_attendence.viewEmployee_attendence);
+app.post('/attendence/add', employee_attendence.addAttendence);
 
 module.exports = app;
 app.listen(4000, function() {
--- routes/employee_attendence.js
+++ routes/employee_attendence.js
@@ -1,35 +1,35 @@
 var EmployeeAttendenceSchema = require('../Models/employee_attendence');
-// var mongoose = require("mongoose");
+
 exports.viewEmployee_attendence = function(req, res) {
     res.render('employee_attendence.html', {
         layout: false
     });
-}
+};
+
 exports.addAttendence = function(req, res) {
     console.log(req.body);
-    // var record = new EmployeeAttendenceSchema({
-    //     Employee_Id: req.body.Employee_Id,
-    //     Number_Of_Attendence: req.body.Number_Of_Attendence,
-    //     Absents: req.body.Absents,
-    //     Informed_Absentees: req.body.Informed_Absentees,
-    //     Lates: req.body.Lates,
-    //     Informed_Lates: req.body.Informed_Lates,
-    //     Casual_Leaves: req.body.Casual_Leaves,
-    //     Sick_Leaves: req.body.Sick_Leaves,
-    // });
+    if (req.body) {
+        var record = new EmployeeAttendenceSchema({
+            Employee_Id: req.body.Employee_Id,
+            Number_Of_Attendence: req.body.Number_Of_Attendence,
+            Absents: req.body.Absents,
+            Informed_Absentees: req.body.Informed_Absentees,
+            Lates: req.body.Lates,
+            Informed_Lates: req.body.Informed_Lates,
+            Casual_Leaves: req.body.Casual_Leaves,
+            Sick_Leaves: req.body.Sick_Leaves,
+        });
+        record.save(function(err, doc) {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            } else {
+                console.log(doc);
+                res.send(doc);
+            }
+        });
+    } else {
+        console.log('body is empty');
+    }
 
-
-    // record.save(function(err, doc) { //record.save (callback)
-    // 	//function (err, doc)
-    //     if (err) {
-    //         res.send(err);
-    //     } else {
-    //         return console.error(err);
-    //         console.log('saved : ' + doc);
-    //         //console.dir(Asad);
-    //         res.send(doc);
-
-    //     }
-    // });
-    // res.send(req.body);
-}
+};
